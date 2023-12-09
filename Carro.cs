94dcbd96c8d5f8b41f6cde02919b89dccee828c6
Marcos S. Ferreira <msferreira.prof@gmail.com>
2023-12-09 18:31:33 -0300

    Módulo 2 - Desafio: hora da prática

--- Carro.cs
+++ Carro.cs
@@ -1,8 +1,30 @@
 ﻿public class Carro
 {
-    public string placa;
-    public string modelo;
-    public double anoFabricacao;
+    private int ano;
+    public string Modelo { get; set; }
+    public string Fabricante { get; set; }
+
+    // exemplo utilizando lambda para get e set
+    // ano do carro deve ser >= 1960 e <= 2023
+    public int AnoFabricacao
+    {
+        get => ano;
+        set
+        {
+            if (value < 1960 || value > 2023)
+            {
+                Console.WriteLine("Valor inválido, insira um ano entre 1960 e 2023");
+            }
+            else
+            {
+                ano = value;
+            }
+
+        }
+    }
+    public string DescricaoDetalhada => $"O fabricante {Fabricante} produziu o modelo {Modelo} em {AnoFabricacao}";
+
+
 
     public void Acelerar()
     {
